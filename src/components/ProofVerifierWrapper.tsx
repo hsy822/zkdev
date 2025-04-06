b9e53b95e6730fec37059cd304f6a5f82997e8e4
hsy822 <hyunsy822@gmail.com>
2025-04-06 14:21:34 +0900

    use wrapper

--- src/components/ProofVerifierWrapper.tsx
+++ src/components/ProofVerifierWrapper.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
-import ProofVerifier from './ProofVerifier';
 
 export default function ProofVerifierWrapper() {
   return (
     <BrowserOnly fallback={<div>Loading verifier...</div>}>
       {() => {
-        // const ProofVerifier = require('./ProofVerifier').default;
-        // console.log(ProofVerifier)
+        const ProofVerifier = require('./ProofVerifier').default;
         return <ProofVerifier />;
       }}
     </BrowserOnly>
